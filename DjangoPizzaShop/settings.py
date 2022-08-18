6e030ce74167ab7faf53b8ebdead30f775860d21
Lev Slinsen <lev_slinsen@protonmail.com>
2022-08-18 17:59:49 +0200

    removed logging

--- DjangoPizzaShop/settings.py
+++ DjangoPizzaShop/settings.py
@@ -183,29 +183,3 @@ if DEBUG:
 ABSOLUTEURI_PROTOCOL = 'https'
 
 SITE_ID = 1
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
-        }
-    },
-    'handlers': {
-        'gunicorn': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'verbose',
-            'filename': BASE_DIR + '/gunicorn.errors',
-            'maxBytes': 1024 * 1024 * 100,  # 100 mb
-        }
-    },
-    'loggers': {
-        'gunicorn.errors': {
-            'level': 'DEBUG',
-            'handlers': ['gunicorn'],
-            'propagate': True,
-        },
-    }
-}
